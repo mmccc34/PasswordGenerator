ae520d0d29ac2c68d1be934dfcbab797a05126d0
mmccc34 <mmccc34@icloud.com>
2024-11-07 22:02:25 +0100

    envoi webpack config

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ use Chris\PasswordGenerator\PasswordGenerator;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/main.css">
     <title>Document</title>
 </head>
 
